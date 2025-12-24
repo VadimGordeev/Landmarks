8d8a8a7e47ce118a9dfad68fdfc323f22dd161ed
VadimGordeev <vadimgordeev22@gmail.com>
2025-12-24 13:28:10 +0100

    feat: first lesson

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vadim on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
